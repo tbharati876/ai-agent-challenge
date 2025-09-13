848e6eb71eddd6f0853505dff8123a5e357621a3
Topireddy Bharathi <tbharati583@gmail.com>
2025-09-13 11:41:45 +0530

    test_icici.py

--- tests/test_icici.py
+++ tests/test_icici.py
@@ -5,7 +5,8 @@ import pandas as pd
 from custom_parsers.icici_parser import parse
 
 def test_icici():
-    expected = pd.read_csv("data/icici/results.csv")
-    actual = parse("data/icici/icici_sample.pdf")  # keep PDF path here
+    expected = pd.read_csv("data/icici/result.csv")
+    actual = parse("data/icici/icici sample.pdf")  # space in filename
     assert actual.equals(expected)
+
 
