843a7a0bb0e2c2832e84575d2cf955278ef67ea0
Topireddy Bharathi <tbharati583@gmail.com>
2025-09-13 11:35:04 +0530

    test_icici.py

--- tests/test_icici.py
+++ tests/test_icici.py
@@ -1,7 +1,11 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import pandas as pd
 from custom_parsers.icici_parser import parse
 
-def test_icici_parser():
+def test_icici():
     expected = pd.read_csv("data/icici/icici_sample.csv")
     actual = parse("data/icici/icici_sample.pdf")
     assert actual.equals(expected)
+
